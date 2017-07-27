aeeb6d04fd38f258376727765bad1ba8c67fc4d4
Krew <wanhao.huang@outlook.com>
2017-07-27 19:18:43 +0800

    修复Trending入口节点消失 (#5)

--- res/js/contents/trending.js
+++ res/js/contents/trending.js
@@ -2,14 +2,14 @@
 
 
 function addTrendingLink() {
-	var menu_nav = document.querySelector('ul.header-nav.float-left');
+	var menu_nav = document.querySelector('.flex-justify-between:first-child ul.list-style-none');
 	/*
 	<li class="header-nav-item">
 	    <a href="/pulls" class="js-selected-navigation-item header-nav-link" data-ga-click="Header, click, Nav menu - item:pulls context:user" data-hotkey="g p" data-selected-links="/pulls /pulls/assigned /pulls/mentioned /pulls">Pull requests</a>        
 	</li>
 	*/
 	if (menu_nav) {
-		var trending = tag('li', 'header-nav-item', '<a href="/trending" class="js-selected-navigation-item header-nav-link">Trending</a>');
+		var trending = tag('li', 'header-nav-item', '<a href="/trending" class="js-selected-navigation-item header-navlink">Trending</a>');
 		menu_nav.appendChild(trending);
 	}
 }
