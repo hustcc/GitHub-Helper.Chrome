3c41b81065c5744f83ee9789a140f81cf49bb6bc
Jovey Zheng <jovey.zheng@gmail.com>
2019-04-11 11:20:08 +0800

    fix: trending button disappear

--- res/js/contents/trending.js
+++ res/js/contents/trending.js
@@ -2,14 +2,15 @@
 
 
 function addTrendingLink() {
-	var menu_nav = document.querySelector('.flex-justify-between:first-child ul.list-style-none');
+	var menu_nav = document.querySelector('.Header nav');
 	/*
 	<li class="header-nav-item">
 	    <a href="/pulls" class="js-selected-navigation-item header-nav-link" data-ga-click="Header, click, Nav menu - item:pulls context:user" data-hotkey="g p" data-selected-links="/pulls /pulls/assigned /pulls/mentioned /pulls">Pull requests</a>        
 	</li>
 	*/
 	if (menu_nav) {
-		var trending = tag('li', 'header-nav-item', '<a href="/trending" class="js-selected-navigation-item HeaderNavlink px-2">Trending</a>');
+		var trending = tag('a', 'js-selected-navigation-item Header-link  mr-3', 'Trending');
+		trending.href = '/trending'
 		menu_nav.appendChild(trending);
 	}
 }
